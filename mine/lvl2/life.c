2af653e3185fbd3b990e1f129b269253b6146077
hi0ki <ansarimehdi518@gmail.com>
2025-08-19 13:33:51 +0100

    from school

---
+++ mine/lvl2/life.c
@@ -0,0 +1,92 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main(int ac, char **av)
+{
+    if (ac != 4)
+        return (1);
+    int width = atoi(av[1]);
+    int height = atoi(av[2]);
+    int iterations = atoi(av[3]);
+
+    int arr[2][width + 2][height + 2];
+    for (int i = 0; i < 2; i++)
+    {
+        for (int h = 0; h < height + 2 ; h++)
+        {
+            for (int w = 0; w < width + 2; w++)
+            {
+                arr[i][w][h] = 0;
+            }
+        }
+    }
+
+    char cmd;
+    int x = 1;
+    int y = 1;
+    int pen = 0;
+
+    while (read(0, &cmd, 1) > 0)
+    {
+        if (cmd == 'w' && y > 1)
+            y--;
+        else if (cmd == 'a' && x > 1)
+            x--;
+        else if (cmd == 's' && y < height)
+            y++;
+        else if (cmd == 'd' && x < width)
+            x++;
+        else if (cmd == 'x')
+            pen = !pen;
+        if (pen)
+            arr[0][x][y] = 1;
+    }
+
+    for (int iter = 0; iter < iterations; iter++)
+    {
+        for (int hei = 1; hei < height + 1; hei++)
+        {
+            for (int wid = 1; wid < width + 1; wid++)
+            {
+                int nb = 0;
+                for (int y = -1; y <= 1; y++)
+                {
+                    for (int x = -1; x <=1; x++)
+                    {
+                        if (!(x==0 && y==0))
+                            nb += arr[iter%2][wid + x][hei + y];
+                    }
+                }
+
+                if (arr[iter % 2][wid][hei] == 1)
+                {
+                    if (nb == 2 || nb == 3)
+                        arr[(iter + 1) % 2][wid][hei] = 1;
+                    else
+                        arr[(iter + 1) % 2][wid][hei] = 0;
+                }
+                else
+                {
+                    if (nb == 3)
+                    {
+                        arr[(iter + 1) % 2][wid][hei] = 1;
+                    }
+                    else
+                        arr[(iter + 1) % 2][wid][hei] = 0;
+                }
+            }
+        }
+    }
+    
+
+    for (int h = 1; h < height + 1; h++)
+    {
+        for (int w = 1; w < width + 1; w++)
+        {
+            putchar(arr[iterations % 2][w][h] ? 'O' : ' ');
+        }
+        putchar('\n');
+    }
+}
+
